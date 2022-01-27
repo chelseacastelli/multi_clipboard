ec8a8675577be031fefd315599ad535ac6d1d338
Chelsea Castelli <chel.castel@aol.com>
2022-01-27 15:22:01 -0500

    implement function to create json file

--- multi_clipboard.py
+++ multi_clipboard.py
@@ -3,8 +3,21 @@ import sys
 import clipboard
 import json
 
-# Paste data from clipboard to `data`
-data = clipboard.paste()
+def save_items(filepath, data):
+    # Override file is it already exists, otherwise create it
+    with open(filepath, "w") as f:
+        # Dump json data to f (file)
+        json.dump(data, f)
 
-# Save to clipboard
-clipboard.copy('abc')
+save_items("test.json", {"key": "value"})
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    
+    if command == 'save': print('save')
+    elif command == 'load': print('load')
+    elif command == 'list': print('list')
+    else: print('Unknown command')
+
+else:
+    print('Please pass exactly one command...(save/load/list)')
